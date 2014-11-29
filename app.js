aab144f80596f2064a6cfb796972469d0ed6bca3
Hacko <hackohackob@gmail.com>
2014-11-29 14:44:58 +0200

    29.11 14:14 - fix, normal sidebar menu

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -39,10 +39,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-
-// █▀▀▀  █▀▀█  █▀▀█  █▀▀▀█  █▀▀█  █▀▀▀█ 
-// █▀▀▀  █▄▄▀  █▄▄▀  █   █  █▄▄▀  ▀▀▀▄▄ 
-// █▄▄▄  █  █  █  █  █▄▄▄█  █  █  █▄▄▄█ 
 
 // error handlers
 
